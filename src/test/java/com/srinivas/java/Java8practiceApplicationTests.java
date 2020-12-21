21521864162d5d82fd863901e6543f12710c896e
Srinivas Natikar <Srinivas.Natikar@msg-global.com>
2020-12-21 20:27:26 +0530

    Initial Commit

---
+++ src/test/java/com/srinivas/java/Java8practiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srinivas.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8practiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
